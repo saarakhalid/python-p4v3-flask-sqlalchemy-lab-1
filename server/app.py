9bc0b83e12d7be1b95972799108a33bda5590593
SaaraKhalid <khalidsaara6@gmail.com>
2024-04-07 14:17:10 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -22,6 +22,41 @@ def index():
 
 # Add views here
 
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+
+    if earthquake:
+        body = {'id': earthquake.id,
+               'location': earthquake.location,
+               'magnitude': earthquake.magnitude,
+               'year':earthquake.year} # type: ignore
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+
+    return make_response(body, status)
+
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def magnitude_filter(magnitude):
+    specearthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
+
+    count = len(specearthquakes)
+
+    quakes = []
+    for specearthquake in specearthquakes:
+        quakes.append({
+            'id': specearthquake.id,
+            'location': specearthquake.location,
+            'magnitude': specearthquake.magnitude,
+            'year': specearthquake.year
+        })
+
+    body = {'count': count, 'quakes': quakes}
+    return make_response(body, 200)
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
